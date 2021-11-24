5d5d5d9aed7ced20694f240320e1a31c9b7c748e
Broulf <66291288+Broulf@users.noreply.github.com>
2021-11-24 01:03:17 -0600

    Update whitelist.py

--- src/whitelist.py
+++ src/whitelist.py
@@ -6,24 +6,29 @@ import json
 import uuid
 import os
 
-# Opens the TXT that contains the names
-names_list = open("names.txt", "r")
+def main():
+    # Opens the TXT that contains the names
+    names_list = open("names.txt", "r")
+    
+    # Storage for dictionary
+    whitelist = []
+    
+    for line in names_list:
+        # Creates and API requests for each name and strips the names and UUIDs
+        api_request = requests.get("https://api.mojang.com/users/profiles/minecraft/" + line.rstrip())
+        #print(api_request.status_code)
+        #print(api_request.text)
+    
+        # If the API request is successful, add the name and UUID to the dictionary
+        if api_request.status_code == 200:
+            load_json = json.loads(api_request.text)
+            whitelist.append({
+                "uuid": str(uuid.UUID(load_json['id'])),
+                "name": load_json['name']
+            })
+    
+    # Writes the dictionary with the help of os to pipe it into whitelist.json
+    print(json.dumps(whitelist))
+    os.system("python whitelist.py > whitelist.json")
 
-# Storage for dictionary
-whitelist = []
-
-for line in names_list:
-    # Creates and API requests for each name and strips the names and UUIDs
-    api_request = requests.get("https://api.mojang.com/users/profiles/minecraft/" + line.rstrip())
-    #print(api_request.text)
-    load_json = json.loads(api_request.text)
-
-    # Creates a dictionary for each name and UUID
-    whitelist.append({
-        "uuid": str(uuid.UUID(load_json['id'])),
-        "name": load_json['name']
-    })
-
-# Writes the dictionary with the help of os to pipe it into whitelist.json
-print(json.dumps(whitelist))
-os.system("python whitelist.py > whitelist.json")
+main()
